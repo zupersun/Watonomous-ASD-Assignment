6d4fbada406a0febc7a2b5b0207c237a7510ddff
Jasper Sun <223296523+zupersun@users.noreply.github.com>
2026-09-20 14:43:02 -0400

    Subscribe map memory node to /costmap

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -2,7 +2,7 @@
 #define MAP_MEMORY_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "map_memory_core.hpp"
 
 class MapMemoryNode : public rclcpp::Node {
@@ -11,6 +11,12 @@ class MapMemoryNode : public rclcpp::Node {
 
   private:
     robot::MapMemoryCore map_memory_;
+
+    void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
+
+    nav_msgs::msg::OccupancyGrid latest_costmap_;
+    bool costmap_received_ = false;
 };
 
 #endif 
--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -1,6 +1,13 @@
 #include "map_memory_node.hpp"
 
-MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())) {}
+MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemoryCore(this->get_logger())) {
+  costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("/costmap", 10, std::bind(&MapMemoryNode::costmapCallback, this, std::placeholders::_1));
+}
+
+void MapMemoryNode::costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+  latest_costmap_ = *msg;
+  costmap_received_ = true;
+}
 
 int main(int argc, char ** argv)
 {
