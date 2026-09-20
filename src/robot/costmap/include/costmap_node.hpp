2c484886bd219164f3e0ac2b8f55ef746e796636
Jasper Sun <223296523+zupersun@users.noreply.github.com>
2026-09-20 00:49:33 -0400

    Publish costmap as OccupancyGrid

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -5,6 +5,7 @@
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "costmap_core.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
  
 class CostmapNode : public rclcpp::Node {
   public:
@@ -20,6 +21,7 @@ class CostmapNode : public rclcpp::Node {
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
 };
  
 #endif 
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -8,6 +8,7 @@ CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->
   string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
   timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
   lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::lidarCallback, this, std::placeholders::_1));
+  costmap_pub_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/costmap", 10);
   costmap_.initializeGrid();
   RCLCPP_INFO(this->get_logger(), "Costmap ready: %d x %d at %.2f m/cell",
   costmap_.getWidth(), costmap_.getHeight(), costmap_.getResolution());
@@ -38,6 +39,20 @@ void CostmapNode::lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg
   }
 
   costmap_.inflateObstacle();
+
+  nav_msgs::msg::OccupancyGrid grid_msg;
+
+  grid_msg.header.stamp = msg->header.stamp;
+  grid_msg.header.frame_id = msg->header.frame_id;
+  grid_msg.info.resolution = costmap_.getResolution();
+  grid_msg.info.width = costmap_.getWidth();
+  grid_msg.info.height = costmap_.getHeight();
+  grid_msg.info.origin.position.x = -costmap_.getWidth() * costmap_.getResolution() / 2.0;
+  grid_msg.info.origin.position.y = -costmap_.getHeight() * costmap_.getResolution() / 2.0;
+  grid_msg.info.origin.orientation.w = 1.0;
+  grid_msg.data = costmap_.getGrid();
+
+  costmap_pub_->publish(grid_msg);
 
   RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "marked %d of %zu beams", marked, msg->ranges.size());
 }
