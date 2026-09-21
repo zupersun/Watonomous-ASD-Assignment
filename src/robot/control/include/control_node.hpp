de686ab396e05bdc5d5f905c052f15105f007964
Jasper Sun <223296523+zupersun@users.noreply.github.com>
2026-09-20 22:49:22 -0400

    Wire up control node subscribers, publisher and   timer

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -2,7 +2,9 @@
 #define CONTROL_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include "control_core.hpp"
 
 class ControlNode : public rclcpp::Node {
@@ -11,6 +13,25 @@ class ControlNode : public rclcpp::Node {
 
   private:
     robot::ControlCore control_;
+
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void controlLoop();
+
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    nav_msgs::msg::Path current_path_;
+    bool path_received_ = false;
+
+    double robot_x_ = 0.0, robot_y_ = 0.0, robot_yaw_ = 0.0;
+    bool odom_received_ = false;
+
+    double lookahead_distance_ = 1.0;
+    double goal_tolerance_ = 0.1;
+    double linear_speed_ = 0.5;
 };
 
 #endif
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -1,6 +1,27 @@
 #include "control_node.hpp"
+#include <cmath>
 
-ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {}
+ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {
+  path_sub_ = this->create_subscription<nav_msgs::msg::Path>("/path", 10, std::bind(&ControlNode::pathCallback, this, std::placeholders::_1));
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom/filtered", 10, std::bind(&ControlNode::odomCallback, this, std::placeholders::_1));
+  cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+  timer_ = this->create_wall_timer(std::chrono:;miliseconds(100), std::bind(&ControlNode::controlLoop, this));
+}
+
+void ControlNode::pathCallback(const nav_msgs::msg::Path::SharedPtr msg) {
+  current_path_ = *msg;
+  path_received_ = true;
+}
+
+void ControlNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
+  robot_x_ = msg->pose.pose.position.x;
+  robot_y_ = msg->pose.pose.position.y;
+  odom_received_ = true;
+}
+
+void ControlNode::controlLoop() {
+  RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 2000, "path:%d (%zu pts) odom:%d", path_received_, current_path_.poses.size(), odom_received_);
+}
 
 int main(int argc, char ** argv)
 {
